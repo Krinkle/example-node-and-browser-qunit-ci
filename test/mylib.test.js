dd9d45e64c19e39ef67cf7a074a600e71a73fd13
Keith Cirkel <github@keithcirkel.co.uk>
2012-02-10 23:22:37 +0000

    Add some failing tests to see a genuine failure of the build

--- test/mylib.test.js
+++ test/mylib.test.js
@@ -12,3 +12,10 @@ QUnit.test('mylib returns true', function () {
     equal(mylib(), true, 'mylib() returns true');
 
 });
+
+QUnit.test('a failing test', function () {
+	expect(2);
+	equal('This string is not the same', 'as this one');
+
+	equal({ one : 1, two: 3 }, { one: 1, two: 2 }, 'this one\'s gonna fail');
+});
