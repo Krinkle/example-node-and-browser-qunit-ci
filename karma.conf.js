c15cead883ddbab09983f30b17917c944986ee85
Timo Tijhof <krinkle@fastmail.com>
2023-04-07 23:39:39 -0700

    Update for 2022: Fix deprecations in GitHub CI and Coveralls
    * GitHub CI implements checkout and cache steps as versioned
      user-facing software, hence deprecation and upgrade churn for
      `actions/checkout@v2`. It's almost like Microsoft is intentionally
      creating unusable software for their competitors to use, to then
      sell you "AI" to get halfway back to normal levels of productivity.
    
    * node-coveralls has tons of dependencies as is average for npm-hosted
      packages. It appears no longer maintained and depends on the
      abandoned 'request' package, which npm-audit marks as insecure.
    
      Switch to the more mature php-coveralls instead,
      <https://github.com/php-coveralls/php-coveralls>.
    
    * Fix bug in karma config that prevented `npm run test-browser-watch`
      from actually watching instead of doing a single run.
    
    * Switch images to Ubuntu 22.04 LTS.

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,10 @@
 /* eslint-env node */
-
 module.exports = function (config) {
   config.set({
     browsers: [
-      'FirefoxHeadless',
-      // Consider Chromium and Google Chrome to be similar enough.
-      // Pick whichever is most likely to be locally installed and kept up-to-date
-      // (Chromium on Linux, Chrome on macOS/Windows).
-      // You can override this via CHROME_BIN or CHROMIUM_BIN.
-      // For example, if your OS defaults to Chrome but you prefer Chromium,
-      // then set something like CHROME_BIN=/usr/bin/chromium.
-      (process.platform === 'linux') ? 'ChromiumHeadless' : 'ChromeHeadless'
+      'FirefoxHeadless'
+      // 'ChromiumHeadless'
+      // 'ChromeHeadless'
     ],
     frameworks: ['qunit'],
     files: [
